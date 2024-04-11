cb6b2f4a8fd06a038da40041ed14221a2e05da15
marcosjrpb <marcosjrpb@hotmail.com>
2024-04-11 09:09:13 -0300

    tela de Cadastro

--- lib/Cadastro.dart
+++ lib/Cadastro.dart
@@ -1,105 +1,191 @@
 import 'package:flutter/material.dart';
 
 class Cadastro extends StatefulWidget {
-  const Cadastro({super.key});
+  const Cadastro({Key? key}) : super(key: key);
 
   @override
   State<Cadastro> createState() => _CadastroState();
 }
 
 class _CadastroState extends State<Cadastro> {
+  // Controllers
+  TextEditingController _controllerNome = TextEditingController();
+  TextEditingController _controllerEmail = TextEditingController();
+  TextEditingController _controllerSenha = TextEditingController();
+
+  // Função para validar campos
+  bool _validarCampos() {
+    String nome = _controllerNome.text;
+    String email = _controllerEmail.text;
+    String senha = _controllerSenha.text;
+
+    // Expressão regular para validar o e-mail
+    RegExp regexEmail = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    RegExp regexSenha = RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$');
+    return nome.isNotEmpty &&
+        nome.length > 4 &&
+        email.isNotEmpty &&
+        senha.isNotEmpty &&
+        senha.length > 6 &&
+        regexEmail.hasMatch(email) &&
+        regexSenha.hasMatch(senha);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cadastro"),
+        backgroundColor: Color(0xFF004D40),
+        title: Text(
+          "Cadastro",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        iconTheme: IconThemeData(color: Colors.white),
       ),
       body: Container(
-        decoration: BoxDecoration(color: Color(0xff075e54)),
+        decoration: BoxDecoration(color: Color(0xFF075E54)),
         padding: EdgeInsets.all(16),
-        child: Center(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height -
+              AppBar().preferredSize.height -
+              50, // Exemplo de margem inferior extra
           child: SingleChildScrollView(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Padding(
                   padding: EdgeInsets.only(bottom: 32),
                   child: Image.asset(
                     "imagens/usuario.png",
-                    width: 200,
-                    height: 150,
+                    width: 180,
+                    height: 130,
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 8),
-                  child: TextField(
-                    autofocus: true,
-                    keyboardType: TextInputType.text,
-                    style: TextStyle(fontSize: 20),
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                      hintText: "Nome: ",
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(32),
-                      ),
+                TextField(
+                  controller: _controllerNome,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    labelText: "Nome",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
                     ),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 8),
-                  child: TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    style: TextStyle(fontSize: 20),
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                      hintText: "E-mail: ",
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(32),
-                      ),
+                SizedBox(height: 16),
+                TextField(
+                  controller: _controllerEmail,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    labelText: "E-mail",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
                     ),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 8),
-                  child: TextField(
-                    obscureText: true,
-                    keyboardType: TextInputType.text,
-                    style: TextStyle(fontSize: 20),
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                      hintText: "Senha: ",
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(32),
-                      ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: _controllerSenha,
+                  obscureText: true,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    labelText: "Senha",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
                     ),
                   ),
                 ),
-                SizedBox(
-                  width: double.infinity, // Define a largura igual à largura total da tela
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Cadastrar",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 22,
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(vertical: 16), // Define o preenchimento vertical do botão
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(32), // Define a borda do botão
-                      ),
-                      backgroundColor: Colors.green, // Cor de fundo do botão
+                SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_validarCampos()) {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            backgroundColor: Colors.white,
+                            title: Text("Sucesso"),
+                            content: const Text(
+                              "Cadastro realizado com sucesso!",
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text("OK"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            backgroundColor: Colors.white,
+                            title: Text("Erro"),
+                            content: const Row(
+                              children: [
+                                Icon(
+                                  Icons.error_outline,
+                                  color: Colors.red,
+                                ),
+                                SizedBox(width: 8),
+                                Flexible(
+                                  child: Text(
+                                    "Por favor, preencha todos os campos corretamente.\n"
+                                        "Nome: mais de 4 caracteres. \n"
+                                        "Email: completo. \n"
+                                        "Senha mais de 6 caracteres.",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text("OK"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  },
+                  child: Text(
+                    "Cadastrar",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
                     ),
                   ),
+                  style: ElevatedButton.styleFrom(
+                    padding:
+                    EdgeInsets.symmetric(vertical: 16), // Define o preenchimento vertical do botão
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32), // Define a borda do botão
+                    ),
+                    backgroundColor: Colors.green, // Cor de fundo do botão
+                  ),
                 ),
-
               ],
             ),
           ),
@@ -108,3 +194,4 @@ class _CadastroState extends State<Cadastro> {
     );
   }
 }
+
